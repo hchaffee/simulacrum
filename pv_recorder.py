f68b354d842e7b231034b7af6fb46d515cdea695
Helen Chaffee <chaf1234@mcc-simul.slac.stanford.edu>
2020-08-24 11:41:11 -0700

    inhibit-read-only

--- pv_recorder.py
+++ pv_recorder.py
@@ -6,8 +6,17 @@ import sys
 import numpy as np
 import pandas as pd
 import zmq
+import random
 
 
+
+#Things to have running via the fastx window:
+# mcc-simul
+# model service cu_hxr
+# bpm service
+# bmag service 
+# env
+# python3 pv_recorder.py fault_data_cu_hxr.csv 
 
 class PVRecorder:
     def __init__(self, read_map_file):
@@ -17,15 +26,20 @@ class PVRecorder:
         with open(read_map_file) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                pv = epics.PV("{dev}:{attr}".format(dev=row['device_name'], attr=row['attribute']))
-                self.read_map.append(([], [], pv))
+                if row['device_name'] == 'BPMS:UNDH:1305':
+                    pv = epics.PV("BPMS:LTUH:990:{attr}".format(dev="BPMS:LTUH:990", attr=row['attribute']))
+                    pass
+                else:
+                    pv = epics.PV("{dev}:{attr}".format(dev=row['device_name'], attr=row['attribute']))
+                    self.read_map.append(([], [], pv))
     
     def read_pvs(self):
         """ Record new values for all the PVs in the CSV file. """
         start = time.time()
+        time.sleep(5)
         for data, timestamps, pv in self.read_map:
             if not pv.connected:
-               # print("Could not connect to {}".format(pv.pvname))
+                #print("Could not connect to {}".format(pv.pvname))
                 continue
             val = pv.get_with_metadata()
             data.append(val['value'])
@@ -53,7 +67,6 @@ if __name__=='__main__':
     to create a fault at some point in time.  When the script is complete,
     it saves the pandas dataframe in an HDF5-formatted file.
     """
-    print(sys.argv)
     r = PVRecorder(sys.argv[1])
     total_time = 30 #seconds
     elapsed_time = 0
@@ -61,10 +74,6 @@ if __name__=='__main__':
     cmd_socket = zmq.Context().socket(zmq.REQ)
     cmd_socket.connect("tcp://127.0.0.1:{}".format(os.environ.get('MODEL_PORT', "12312")))
     fault_generated = False
-##
-    my_pv = epics.PV("BPMS:LI24:801:X")
-    print(my_pv)
-##
     print(f"Beginning recording. {total_time} remaining.")
     start_time = time.time()
     while elapsed_time < total_time:
@@ -73,11 +82,50 @@ if __name__=='__main__':
         elapsed_time = time.time() - start_time
         if not fault_generated and elapsed_time > fault_time:
             # Generate a sudden phase shift in a subbooster phase
-            print(f"Causing fault at timestamp {time.time()}")
-            commands = [f"set ele O_K23_{i} phase_deg_err = 5" for i in range(1,9)]
+            #print(f"Causing fault at timestamp {time.time()}")
+            #Randomize the error parameters: sector, station, phase error
+
+            # Sector & stations come from oracle
+            sector = 23 #random.randrange(22, 27) # the klystron sector
+            error = random.randrange(10, 40)  # the error in klystron phase
+            station = 1
+            #if sector == 21:   #sector 21 is missing a station 2
+            #    station = random.choice([1, 3, 4, 5, 6, 7, 8])
+            #elif sector == 24: #sector 24 is missing stations 7, 8
+            #    station = random.choice([1, 2, 3, 4, 5, 6])
+            #else:              # all other sectors have stations 1 thru 8 inclusive
+            #    station = random.randrange(1,9)
+            print("Sector: ",sector,", Station: ",station,", Error: ",error)
+            commands = [f"set ele O_K{sector}_{station} phase_deg_err = {error}"]
             cmd_socket.send_pyobj({"cmd": "tao_batch", "val": commands})
             result = cmd_socket.recv_pyobj()
             fault_generated = True
     print("Finished recording.  Writing file.")
-    r.dataframe().to_hdf(sys.argv[2], key='df', mode='w')
-    print(f"Done!  File saved as {sys.argv[2]}")
+    filename = f"sector{sector}_station{station}_err{error}.csv"
+    r.dataframe().to_csv(filename)
+    #r.dataframe().to_hdf(filename, key='df', mode='w')
+    print(f"Done!  File saved as {filename}")
+    
+#### comment out the next 5 lines if all stations need to be reset
+    print("Resetting the fault...")
+    reset_commands = [f"set ele O_K{sector}_{station} phase_deg_err = 0"]
+    cmd_socket.send_pyobj({"cmd": "tao_batch", "val": reset_commands})
+    reset_result = cmd_socket.recv_pyobj()  
+    quit()
+#### 
+
+#### The rest of this code resets all klystron phase errors back to 0, if needed
+    print("Now we need to reset the errors in our system...")
+    for sectors in (21,22,23,24,25,26,27,28,29,30):
+        if sectors == 21:   #sector 21 is missing a station 2
+            stations = [1, 3, 4, 5, 6, 7, 8]
+        elif sectors == 24: #sector 24 is missing stations 7, 8
+            stations = [1, 2, 3, 4, 5, 6]
+        else:              # all other sectors have stations 1 thru 8 inclusive
+            stations = [1,2,3,4,5,6,7,8]
+    
+        reset_commands = [f"set ele O_K{sectors}_{i} phase_deg_err = 0" for i in stations]
+        cmd_socket.send_pyobj({"cmd": "tao_batch", "val": reset_commands})
+        reset_result = cmd_socket.recv_pyobj()
+    print("All sectors done resetting")
+    quit()
